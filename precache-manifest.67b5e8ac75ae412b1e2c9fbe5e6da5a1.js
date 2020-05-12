744195acdc7247340282bf79f225c1505d6717a3
themos <themos.kost@yahoo.gr>
2020-05-12 18:35:23 +0200

    Updates

---
+++ precache-manifest.67b5e8ac75ae412b1e2c9fbe5e6da5a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b05cd8b248380db0a1c1d9e7dd4ee430",
+    "url": "/youtube_clone/index.html"
+  },
+  {
+    "revision": "f438b295f2ee421b06d3",
+    "url": "/youtube_clone/static/js/2.b9f90f6e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/youtube_clone/static/js/2.b9f90f6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "306e51cd543bb3c0dd2a",
+    "url": "/youtube_clone/static/js/main.64e3039d.chunk.js"
+  },
+  {
+    "revision": "8673842b170b3bb6d474",
+    "url": "/youtube_clone/static/js/runtime-main.a24de390.js"
+  }
+]);
